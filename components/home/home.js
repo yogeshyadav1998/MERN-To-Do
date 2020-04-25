d3a4fa62571a66411bb74a7f98ce9bb8e7f40e03
yogeshyadav1998 <yogeshbhakli@gmail.com>
2020-04-25 08:14:17 +0530

    home page ready

--- components/home/home.js
+++ components/home/home.js
@@ -1,31 +1,40 @@
 import React, { Component } from 'react';
 
+import { Icon} from 'antd';
+import DatePicker from 'react-custom-date-picker';
+
 class home extends Component{
     render(){
         return(
             <div className="home">
-                <div className="header">
-                    <div className="profilephoto">
-                        profile photo
-                    </div>
-                    <div>
-                        <div className="name">
-                            wellcome name
-                        </div>
-                        <div className="heading1">
-                            checkout your plans for your beautiful life.
-                        </div>
-                    </div>
-                    <div className="login">
-                        <p>SignIN/SignUp</p>
-                    </div>
+                <div className="backgroud-video">
+                    <video autoPlay loop muted className="video" >
+                        <source src='videos/v.mp4' type='video/mp4' />
+                    </video>
                 </div>
-                <div className="function">
+                <div className="content">
+                    <div className="profile-photo">
+                        <img src="https://images.pexels.com/photos/1615776/pexels-photo-1615776.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"></img>
+                    </div>
+                    <div className="name">
+                        <h3>Wellcome , Rozar Skunk</h3>
+                    </div>
+                    <div className="heading1">
+                        <h5>Explore Your Adventurous Plans For Beautiful Life.</h5>
+                    </div>
                     <div className="search">
-
+                        <div className="heading2">
+                            <h4>Search Your Plans</h4>
+                        </div>
+                        <button className="searchweekly hvr1-wobble-horizontal">This Week Plans <Icon type="right" /></button>
+                        <button className="searchbydate hvr1-wobble-horizontal">Search By Date <Icon type="right" /></button>
                     </div>
                     <div className="update">
-
+                        <div className="heading3">
+                            <h4>Update Your Plans</h4>
+                        </div>
+                        <button className="updateweekly hvr1-wobble-horizontal"><Icon type="left" /> Update Week Plans </button>
+                        <button className="updatebydate hvr1-wobble-horizontal"><Icon type="left" /> Update By Date</button>
                     </div>
                 </div>
             </div>
